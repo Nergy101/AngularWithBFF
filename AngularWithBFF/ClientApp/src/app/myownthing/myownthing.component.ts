c45a6f5a1eacb7e19d23154fe4c38af7d43a187a
Christian <Christian.vandijk@student.hu.nl>
2019-12-17 11:13:38 +0100

    initial

---
+++ AngularWithBFF/ClientApp/src/app/myownthing/myownthing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myownthing',
+  templateUrl: './myownthing.component.html',
+  styleUrls: ['./myownthing.component.css']
+})
+export class MyownthingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
